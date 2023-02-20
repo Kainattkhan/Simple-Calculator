406459189a0e540dd3b6a14c7a3a4ae0430eb32c
KainatIbrahim <kainatabrahim@gmail.com>
2023-02-20 20:24:04 +0500

    Added exponent.py file

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from addition import add
 from multiplication import multiply
 from subtraction import subtract
+from exponent import power
 import pandas as pd
 
 if __name__=="__main__":
-    A=14
-    B=20
+    A=6
+    B=5
 
     print(add(A,B))
     print(subtract(A,B))
     print(multiply(A,B))
+    print(power(A,B))
