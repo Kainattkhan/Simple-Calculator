a7b67684c7ec93ab13025cdba21542f1db998563
Kainat <kainattkhan029@gmail.com>
2023-02-20 15:44:36 +0500

    Added main file

---
+++ main.py
@@ -0,0 +1,11 @@
+from addition import add
+from multiplication import multiply
+from subtraction import subtract
+
+if __name__=="__main__":
+    A=14
+    B=20
+
+    print(add(A,B))
+    print(subtract(A,B))
+    print(multiply(A,B))
